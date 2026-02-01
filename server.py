5a878d4fc7fa6b2753a25f79b51136be030efe11
Stauffacher <marcostauffacher@bluewin.ch>
2026-02-01 07:36:09 +0100

    Update server.py

--- server.py
+++ server.py
@@ -7,9 +7,4 @@ def greeting(name: str) -> str:
     return f"Hello, {name}!"
 
 if __name__ == "__main__":
-    mcp.run(
-        "streamable-http",
-        host="0.0.0.0",
-        port=8000,
-    )
-
+    mcp.run("streamable-http")
